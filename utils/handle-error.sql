420ff53468831391811f1dd849335a12b7263118
Duc Manh Nguyen <ndmanh3003@gmail.com>
2024-12-03 13:56:00 +0700

    ge

--- utils/handle-error.sql
+++ utils/handle-error.sql
@@ -7,7 +7,7 @@ CREATE OR ALTER PROCEDURE sp_Validate
     @id1 INT,
     @id2 INT = NULL,
     @id3 INT = NULL
-AS
+AScsfsd
 BEGIN
     IF @type = 'branch' AND NOT EXISTS (SELECT 1 FROM Branch WHERE id = @id1)
         THROW 50000, 'ERR_NO_BRANCH', 1;
