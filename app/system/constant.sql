888bd11c2cd9a3bc96634b36172c821d1bf59b21
phucvu0210 <phucvu0210@gmail.com>
2024-12-10 16:31:29 +0700

    fix create, update system constant

--- app/system/constant.sql
+++ app/system/constant.sql
@@ -1,8 +1,8 @@
 USE SSMORI
 GO
 
--- TODO: Cập nhật hệ thống
-CREATE OR ALTER PROC sp_UpdateSystemConstants  
+-- TODO: Tạo hệ thống
+CREATE OR ALTER PROC sp_CreateSystemConstants  
     @costPerKm DECIMAL(10, 2),
     @freeDistance INT,
     @phone NVARCHAR(15),
@@ -27,5 +27,31 @@ BEGIN
 		@dishSilverDiscount,
 		@dishGoldDiscount
 	)
+END
+GO
+
+-- TODO: Cập nhật hệ thống
+CREATE OR ALTER PROC sp_UpdateSystemConstants  
+    @costPerKm DECIMAL(10, 2) = NULL,
+    @freeDistance INT = NULL,
+    @phone NVARCHAR(15) = NULL,
+	@shipMemberDiscount INT = NULL,
+	@shipSilverDiscount INT = NULL,
+	@shipGoldDiscount INT = NULL,
+	@dishMemberDiscount INT = NULL,
+	@dishSilverDiscount INT = NULL,
+	@dishGoldDiscount INT = NULL
+AS
+BEGIN
+	UPDATE Const
+    SET costPerKm = COALESCE(@costPerKm, costPerKm),
+        freeDistance = COALESCE(@freeDistance, freeDistance),
+        phone = COALESCE(@phone, phone),
+        shipMemberDiscount = COALESCE(@shipMemberDiscount, shipMemberDiscount),
+        shipSilverDiscount = COALESCE(@shipSilverDiscount, shipSilverDiscount),
+    	shipGoldDiscount = COALESCE(@shipGoldDiscount, shipGoldDiscount),
+        dishMemberDiscount = COALESCE(@dishMemberDiscount, dishMemberDiscount),
+        dishSilverDiscount = COALESCE(@dishSilverDiscount, dishSilverDiscount),
+        dishGoldDiscount = COALESCE(@dishGoldDiscount, dishGoldDiscount)
 END
 GO
